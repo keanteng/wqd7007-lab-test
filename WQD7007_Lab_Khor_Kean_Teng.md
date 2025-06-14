10094fe9ae1ea6fc00bf676120b9c1528ab96bbc
Khor Kean Teng <u2004763@siswa.um.edu.my>
2025-06-14 12:42:11 +0800

    update files #3

---
+++ WQD7007_Lab_Khor_Kean_Teng.md
@@ -0,0 +1,7 @@
+---
+title: WQD7007 Lab Test - Khor Kean Teng
+author: Khor Kean Teng
+date: 2025-06-14
+---
+
+# WQD7007 Lab Test - Khor Kean Teng
---
+++ bin/to-doc.sh
@@ -0,0 +1,3 @@
+export PANDOC_PATH="/C/Users/Khor Kean Teng/Downloads/pandoc-3.7.0.2-windows-x86_64/pandoc-3.7.0.2"
+export PATH="$PATH:$PANDOC_PATH"
+pandoc WQD7007_Lab_Khor_Kean_Teng.md -o WQD7007_Lab_Khor_Kean_Teng.docx
